27b3a39421c7f571db4f35bba67d9a3e8c9b2ded
Christopher Dean <Tpimp@users.noreply.github.com>
2020-12-10 14:40:01 -0800

    More Testing for deploy

--- olga2/main.js
+++ olga2/main.js
@@ -8094,8 +8094,8 @@ const environment = {
     production: false,
     rootPath: 'olga2/',
     imagesPath: 'olga2/assets/images/',
-    playersPath: 'olga2/assets/images/players',
-    piecesPath: 'olga2/assets/images/players',
+    playersPath: 'olga2/assets/images/players/',
+    piecesPath: 'olga2/assets/images/pieces/',
     iconsPath: 'olga2/assets/images/icons/',
     flagsPath: 'olga2/assets/images/flags/'
 };
