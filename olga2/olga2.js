5c449d9de5d35d7fe678dcc698b04f74cd4aca9a
Christopher Dean <Tpimp@users.noreply.github.com>
2020-12-10 20:03:04 -0800

    testing olga

--- olga2/olga2.js
+++ olga2/olga2.js
@@ -1,26 +1,31 @@
-var Olga2 = (function() {
-    function Olga2(id, pgn, ) {
+export class Olga2{
+    constructor(id, data = null) {
         if(!id){
             throw new Error('Olga2 Constructor requires an id');
             return;
         }
         this.id = id;
         console.log('Searching for olga instance with id:' + id);
-        if(pgn) {
-            console.log('Pgn: ');
-            console.log(pgn);
+        if(data) {
+            if(data.settings) {
+                console.log(data.settings);
+            }
+            if(data.pgn) {
+                console.log('Pgn: ');
+                console.log(pgn);
+            }
         }
     }
 
 
 
-    function attachEvents() {
+    #attachEvents() {
         window.addEventListener('message', this.receiveMessage.bind(this), false);
     }
 
 
 
-    function receiveMessage(event) {
+    #receiveMessage(event) {
         // get message (json)
         // decode and handle errors
         // check what the message type is, do something
@@ -31,8 +36,8 @@ var Olga2 = (function() {
             }
     }
 
-    Olga2.prototype.setOlgaSettings = function(settings_obj) {
+    setOlgaSettings(settings_obj) {
         console.log('recieved user settings:')
-        console.log(setting_obj);
+        console.log(settings_obj);
     }
-})();
+}
