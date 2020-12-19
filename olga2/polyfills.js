99dbc84fd0d34b36afc332066f9a0fe07207fdc1
Christopher Dean <Tpimp@users.noreply.github.com>
2020-12-18 16:38:25 -0800

    Wow this almost looks like an RC

--- olga2/polyfills.js
+++ olga2/polyfills.js
@@ -3033,7 +3033,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\projects\Olga\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\projects\Aria\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
