227350c6723e5cfcd21346a1e039fa86426f7e1e
Christopher Dean <Tpimp@users.noreply.github.com>
2021-01-27 12:40:43 -0800

    Removing references to postMessage API for aria.js

--- Aria/aria.js
+++ Aria/aria.js
@@ -1,72 +1,21 @@
-const JSRPC = {
-    setPGN: 'setPGN',
-    onSettings: 'onSettings',
-    getSettings: 'getSettings',
-    layoutChanged: 'layoutChanged',
-    setSize: 'setSize'
-}
-
 class Aria {
-    static ATTACHED = false;
-    #attachEvents() {
-        window.addEventListener('message', this.#receiveMessage.bind(this), false);
-        Aria.ATTACHED = true;
-    }
-    #receiveMessage(event) {
-        // get message (json)
-        // decode and handle errors
-        // check what the message type is, do something
-        let message_obj = null;
-        try {
-            message_obj = JSON.parse(event.data);
-        } catch (error) {
-            console.log('Received parsing bad message from Aria frame: ' + this.id);
-            console.log(error);
-            console.log('Errored on Event:');
-            console.log(event);
-        }
-        if (message_obj && message_obj.type) {
-            switch (message_obj.type) {
-                case JSRPC.onSettings: {
-                    if (message_obj.data) {
-                        this.onSettings(message_obj.data);
-                    }
-                    break;
-                }
-                case JSRPC.layoutChanged: {
-                    if (message_obj.data && this.layoutChanged !== null) {
-                        this.layoutChanged(message_obj.data);
-                    }
-                    break;
-                }
-                default: { break; }
-            }
-        }
-        // decode the message, then call the corresponding callback
-    }
-
-    constructor(id) {
-
-        this.element = document.getElementById(id);
+    constructor(id, iFrame = true) {
+        
         let instance = null;
-        if(this.element.contentWindow['ARIA']) {
-            instance = this.element.contentWindow['ARIA'];
-        }
-        if(!(instance.aria && instance.theme && instance.layout)) {
-            throw new Error('Aria Constructor requires an id');
+        if(iFrame) {
+            this.element = document.getElementById(id);
+            if(this.element && this.element.contentWindow['ARIA']) {
+                instance = this.element.contentWindow['ARIA'];
+            }
+        }        
+        if(!(instance && instance.aria && instance.theme && instance.layout)) {
+            throw new Error('Aria Constructor did not find the element with id: ' + id);
             return;
         }
-        if (instance) {
-            this.id = id;
-            this.aria = instance.aria;
-            this.theme = instance.theme;
-            this.layout = instance.layout;
-            if (!Aria.ATTACHED) {
-                this.#attachEvents();
-            }
-        } else {
-            console.log('aria.js: Cannot find iframe by id=' + id);
-        }
+        this.id = id;
+        this.aria = instance.aria;
+        this.theme = instance.theme;
+        this.layout = instance.layout;
     }
     setPGN(pgn) {
         this.aria.loadPGN(pgn);        
@@ -77,15 +26,11 @@ class Aria {
     setTheme(theme){
         this.theme.setSettings(theme);
     }
-    
+
     getSettings() {
         return {theme: this.theme.settings(), aria:this.aria.settings(), layout:this.layout.settings()};
     }
 
-    // callbacks
-
-    onSettings = function (settings) { }
-    onOpenMenu = function (data) { }
     layoutChanged(data) {
         const hPx = data.height + 'px';
         console.log(data);
