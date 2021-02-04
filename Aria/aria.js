984e4062dd14d9bca3dc2d5493b046044072195c
Christopher Dean <Tpimp@users.noreply.github.com>
2021-02-04 14:32:30 -0800

    Example of sending PGN through URL hash

--- Aria/aria.js
+++ Aria/aria.js
@@ -1,39 +1,25 @@
-class Aria {
-    constructor(id, iFrame = true) {
-        
-        let instance = null;
-        if(iFrame) {
-            this.element = document.getElementById(id);
-            if(this.element && this.element.contentWindow['ARIA']) {
-                instance = this.element.contentWindow['ARIA'];
-            }
-        }        
-        if(!(instance && instance.aria && instance.theme && instance.layout)) {
-            throw new Error('Aria Constructor did not find the element with id: ' + id);
-            return;
+function Aria(id, iFrame=true, resize=true) {
+    var instance = null;
+    if(iFrame) {
+        this.element = document.getElementById(id);
+        if(this.element && this.element.contentWindow['ARIA']) {
+            instance = this.element.contentWindow['ARIA'];
         }
-        this.id = id;
-        this.aria = instance.aria;
-        this.theme = instance.theme;
-        this.layout = instance.layout;
-        this.layout.layoutChanged = (width, height, state) =>{
+    }else {
+        if(window['ARIA']) {
+            instance = window['ARIA'];
+        }
+    }        
+    if(!(instance && instance.aria && instance.theme && instance.layout)) {
+        throw new Error('Aria Constructor did not find the element with id: ' + id);
+        return;
+    }
+    if(resize) {
+        instance.layout.layoutChanged = (width, height, state) =>{
             const hPx = height + 'px';
             const current = this.element.style.height;
             this.element.style.height = current != hPx ? hPx:current;
         }
     }
-    setPGN(pgn) {
-        this.aria.loadPGN(pgn);        
-    }
-    setSettings(settings) {
-        this.aria.setSettings(settings);
-    }
-    setTheme(theme){
-        this.theme.setSettings(theme);
-    }
-
-    getSettings() {
-        return {theme: this.theme.settings(), aria:this.aria.settings(), layout:this.layout.settings()};
-    }
-
+    return instance;
 }
